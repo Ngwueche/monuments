40420deb53ad7e0250c72ae7a97de26edb7a6a65
Ngwueche <wisdomngwueche@gmail.com>
2023-06-13 10:32:39 +0100

    set up routes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import  bodyParser from 'body-parser';
-import mongooose from 'mongoose'
+import mongoose from 'mongoose';
+import postRoutes from "./routes/post.js"
+
 
 const app = express();
+//middlewares
+app.use('/post', postRoutes)
 app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
-app.cors(())
+app.use(cors())
+// const pwd = process.env.DB_PWD
+const PORT = process.env.PORT || 3000
+mongoose.connect('mongodb+srv://ngwueche:1zW07qZ0qDeneUsv@cluster0.bltn3gb.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>app.listen(PORT ,()=>console.log('connected and running')))
+.catch((error)=>console.log(error.message));
---
+++ server/routes/post.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { getPost, createPost } from "../controllers/post.js";
+
+const router =  express.Router()
+
+router.get('/', getPost);
+router.get('/', createPost);
+export default router;
